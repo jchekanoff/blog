c03dd74dac2c57858cf5ea75b49d153124a36a17
Joseph Chekanoff <joe@chekcreative.com>
2022-12-03 12:42:47 -0500

    fix: Fathom operational and 404 favicon fixes

--- components/analytics/index.js
+++ components/analytics/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { useRouter } from "next/router"
 
-import Fathom from "fathom-client";
+import * as Fathom from "fathom-client";
 
 import GA from './GoogleAnalytics'
 import Plausible from './Plausible'
