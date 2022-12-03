0cb7df78c9c7f1ea0f1e0c23a72bbbf0af569717
Joseph Chekanoff <joe@chekcreative.com>
2022-12-03 11:45:01 -0500

    feat: Update projectsData and images

--- data/projectsData.js
+++ data/projectsData.js
@@ -4,14 +4,14 @@ const projectsData = [
         description: `The Web Studio Without Limits. Chek Creative replaces the unreliable 
         developer in your network with a flat recurring fee. We promise to exceed your expectations 
         with our agility, performance, and service.`,
-        imgSrc: '/static/images/google.png',
+        imgSrc: '/static/images/chek-creative.jpg',
         href: 'https://chekcreative.com',
     },
     {
         title: 'EventBook',
         description: `The Next Chapter in Event Management Software. Manage and book events 
         from inquiry through invoicing while automating and growing your business.`,
-        imgSrc: '/static/images/time-machine.jpg',
+        imgSrc: '/static/images/eventbook.jpg',
         href: 'https://eventbook.so',
     },
 ]
